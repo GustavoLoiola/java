476b53ce069bc757d45f3f50880bd5479c77376e
GustavoLoiola <gusantanaloiola@gmail.com>
2026-04-12 16:39:29 -0300

    estudando ArrayList e implementando no meu sistema

--- dio06/src/App.java
+++ dio06/src/App.java
@@ -1,42 +1,75 @@
 import java.util.Scanner;
-
+import java.lang.reflect.Array;
+import java.util.AbstractList;
+import java.util.ArrayList;
 
 public class App {
+
+    static Scanner entrada = new Scanner(System.in);
+    static ArrayList<Pessoa> pessoas = new ArrayList<>();
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        while (true) {
-             System.out.println("MENU PRINCIPAL");
-             System.out.printf("1 - Cadastrar Pessoa / 2 - Ver Pessoas / 0 - Sair");
-             int num = entrada.nextInt();
-             switch (num) {
-                case 1 -> {System.out.println("Digite o nome: ");
-                String nome = entrada.nextLine();
 
-                System.out.println("Digite o CPF: ");
-                int cpf = entrada.nextInt();
-
-                System.out.println("Digite a idade: ");
-                int idade = entrada.nextInt();
-
-                System.out.println("Digite a nacionalidade: ");
-                String nacionalidade = entrada.nextLine();
-
-                }
-                case 2 -> System.out.println("teste2");
-                case 0 -> {
-                    break;
-                }
-                default -> System.out.println("Opção inválida!");
-             }
-        System.out.println("Fim do Programa.");
-        }
+        Menu();
        
-        /*pessoa pessoa1 = new pessoa();
-            pessoa1.idade = 3;
-           
-            pessoa1.cpf = 1234567;
-            pessoa1.nacionalidade = "Brasil";
-            pessoa1.cumprimentar(); */
-        
+        System.out.println("Fim do Programa.");
+        entrada.close();
     }
+
+        public static void Cadastrar() {
+
+            entrada.nextLine();
+
+            System.out.println("Digite o nome: ");
+            String nomeCadastro = entrada.nextLine();
+
+            System.out.println("Digite a idade: ");
+            int idadeCadastro = entrada.nextInt();
+
+            entrada.nextLine();
+
+            System.out.println("Digite o CPF: ");
+            String cpfCadastro = entrada.nextLine();
+
+
+            System.out.println("Digite a nacionalidade: ");
+            String nacionalidadeCadastro = entrada.nextLine();
+
+            Pessoa pessoa = new Pessoa(nomeCadastro, idadeCadastro, cpfCadastro, nacionalidadeCadastro);
+
+            pessoas.add(pessoa);
+
+            System.out.println("Pessoa Cadastrada!");
+        }
+
+        public static void Exibir() {
+           if(pessoas.isEmpty()) {
+                System.out.println("Não existem usuários cadastrados!");
+           }
+           else {
+                for(Pessoa i : pessoas) {
+                    i.mostrarDados();
+                    System.out.println("-------------------------------------------------------");
+                }
+           }
+        }
+
+
+         public static void Menu() {
+            while (true) {
+                System.out.println("MENU PRINCIPAL");
+            
+                System.out.println("1 - Cadastrar Pessoa / 2 - Ver Pessoas / 0 - Sair");
+                int num = entrada.nextInt();
+
+                
+                switch (num) {
+                    case 1 -> Cadastrar();
+                    case 2 -> Exibir();
+                    case 0 -> {
+                        return;
+                    }
+                    default -> System.out.println("Opção inválida!");
+             }
+            }
+        }
 }
--- dio06/src/pessoa.java
+++ dio06/src/pessoa.java
@@ -1,10 +1,22 @@
-public class pessoa {
-    int idade;
-    String nome;
-    int cpf;
-    String nacionalidade;
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String cpf;
+    private String nacionalidade;
 
-    void cumprimentar() {
-        System.out.println("Olá!");
+
+    public Pessoa(String nome, int idade, String cpf, String nacionalidade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.nacionalidade = nacionalidade;
+    }
+
+    void mostrarDados() {
+        System.out.println("PESSOA");
+        System.out.printf("Nome:  %s\n", nome);
+        System.out.printf("Idade:  %d\n", idade);
+        System.out.printf("CPF:  %s\n", cpf);
+        System.out.printf("Nacionalidade:  %s\n", nacionalidade);
     }
 }
