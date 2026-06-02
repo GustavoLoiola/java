d0838c12c9b786c76f2fc64a7db581dd4d26d7fc
GustavoLoiola <gusantanaloiola@gmail.com>
2026-06-02 18:02:57 -0300

    preparando o menu do jogo

--- dio28/src/br/com/dio/Main.java
+++ dio28/src/br/com/dio/Main.java
@@ -1,7 +1,74 @@
 package br.com.dio;
+import br.com.dio.model.Board;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
 
+    private final static Scanner scanner = new Scanner(System.in);
+
+    private static Board board;
+
+    private final static int BOARD_LIMIT = 9;
+
+    public static void main(String[] args) {
+        final var positions = Stream.of(args)
+                .collect(Collectors.toMap(
+                        k -> k.split(";")[0],
+                        v -> v.split(";")[1]
+                ));
+
+        var option = -1;
+
+        while(true) {
+            System.out.println("SUDOKU - DIGITE UMA DAS OPÇÕES A SEGUIR: ");
+            System.out.println("1 - Iniciar novo jogo");
+            System.out.println("2 - Adicionar número ");
+            System.out.println("3 - Remover número");
+            System.out.println("4 - Visualizar jogo atual");
+            System.out.println("5 - Verificar status do jogo");
+            System.out.println("6 - Limpar jogo");
+            System.out.println("7 - Finalizar jogo");
+            System.out.println("8 - Sair");
+
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1 -> startGame(positions);
+                case 2 -> inputNumber();
+                case 3 -> removeNumber();
+                case 4 -> showCurrentGame();
+                case 5 -> showGameStatus();
+                case 6 -> clearGame();
+                case 7 -> finishGame();
+                case 8 -> System.exit(0);
+                default -> System.out.println("Opção inválida! Por favor, tente novamente.");
+            }
+        }
     }
+
+    private static void startGame(Map<String, String> positions) {
+    }
+
+    private static void finishGame() {
+    }
+
+    private static void clearGame() {
+    }
+
+    private static void showGameStatus() {
+    }
+
+    private static void showCurrentGame() {
+    }
+
+    private static void removeNumber() {
+    }
+
+    private static void inputNumber() {
+    }
+
 }
