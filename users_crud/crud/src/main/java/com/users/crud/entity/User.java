403779b78933699be7008f2e653ee31a3a166f2f
GustavoLoiola <gusantanaloiola@gmail.com>
2026-07-24 16:30:17 -0300

    Adicionando roles e password ao meu user

--- users_crud/crud/src/main/java/com/users/crud/entity/User.java
+++ users_crud/crud/src/main/java/com/users/crud/entity/User.java
@@ -1,12 +1,17 @@
 package com.users.crud.entity;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.*;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,18 +19,34 @@ public class User {
 
     private String name;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 
     private LocalDate birthDate;
 
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+
     public User() {}
 
-    private User(Long id, String name,String email, LocalDate birthDate){
+    public User(Long id, String name,String email, LocalDate birthDate, String password, UserRole role){
         this.id = id;
         this.name = name;
         this.email = email;
         this.birthDate = birthDate;
+        this.password = password;
+        this.role = role;
     }
 
     public Long getId() {
@@ -58,5 +79,26 @@ public class User {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
     }
 }
