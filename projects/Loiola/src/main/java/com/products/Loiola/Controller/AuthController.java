784299e62364372de8ff12cb1fbc4300662c0bed
GustavoLoiola <gusantanaloiola@gmail.com>
2026-09-04 19:00:24 -0300

    adicionando validações simples para usuário
    validações simples para cadastro de usuário

--- projects/Loiola/src/main/java/com/products/Loiola/Controller/AuthController.java
+++ projects/Loiola/src/main/java/com/products/Loiola/Controller/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+
 public class AuthController {
 
     @Autowired
@@ -43,7 +45,7 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody RegisterRequest data) {
+    public ResponseEntity<String> register(@Valid @RequestBody RegisterRequest data) {
         System.out.println("--> DADOS CHEGANDO NO REQUEST: " + data);
 
         if (this.userRepository.findByEmail(data.email()).isPresent()) {
--- projects/Loiola/src/main/java/com/products/Loiola/DTO/RegisterRequest.java
+++ projects/Loiola/src/main/java/com/products/Loiola/DTO/RegisterRequest.java
@@ -1,6 +1,26 @@
 package com.products.Loiola.DTO;
 
 import com.products.Loiola.Model.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
-public record RegisterRequest(String name, String email,  String password, String phone,UserRole role) {
+public record RegisterRequest(
+        @NotBlank(message = "O nome é obrigatório")
+        @Size(min = 3, message = "O nome deve ter pelo menos 3 letras")
+        String name,
+        @NotBlank(message = "O email é obrigatório!")
+        @Email
+        String email,
+        @NotBlank(message = "A senha é obrigatória!")
+        @Pattern(
+                regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).*$",
+                message = "A senha deve conter pelo menos uma letra maiúscula, uma minúscula, um número e um caractere especial"
+        )
+        String password,
+        @Size(min = 11, max = 11, message = "O número de telefone deve conter exatamente 11 números incluindo o ddd.")
+        String phone,
+        UserRole role) {
+
 }
