0924178e2241540498390c0b13883e09eae71b8b
GustavoLoiola <gusantanaloiola@gmail.com>
2026-09-07 18:41:16 -0300

    criando exceptions personalizadas
    criando GlobalExceptionHandler e InvalidUserException e substituindo tratamentos antigos no meu service

--- projects/Loiola/src/main/java/com/products/Loiola/Service/AuthService.java
+++ projects/Loiola/src/main/java/com/products/Loiola/Service/AuthService.java
@@ -5,15 +5,17 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.products.Loiola.DTO.RegisterRequest;
+import com.products.Loiola.Exceptions.InvalidUserException;
 import com.products.Loiola.Model.User;
+import com.products.Loiola.Model.UserRole;
 import com.products.Loiola.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -24,22 +26,39 @@ public class AuthService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Value("${api.security.token.secret:my-secret-key}")
     private String secret;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return (UserDetails) userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + email));
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new  UsernameNotFoundException("Usuário não encontrado: " + email));
     }
 
     public void register(RegisterRequest data) {
 
         if (userRepository.findByEmail(data.email()).isPresent()) {
-            throw new RuntimeException(
+            throw new InvalidUserException(
                     "Já existe um usuário com esse Email cadastrado no sistema."
             );
         }
+
+        String encryptedPassword = passwordEncoder.encode(data.password());
+
+        User newUser = new User();
+
+        newUser.setName(data.name());
+        newUser.setEmail(data.email());
+        newUser.setPassword(encryptedPassword);
+        newUser.setPhone(data.phone());
+
+        newUser.setRole(UserRole.USER);
+
+        userRepository.save(newUser);
+
     }
 
         public String generateToken(User user) {
@@ -51,7 +70,7 @@ public class AuthService implements UserDetailsService {
                     .withExpiresAt(generateExpirationDate())
                     .sign(algorithm);
         } catch (JWTCreationException exception) {
-            throw new RuntimeException("Erro ao gerar token JWT", exception);
+            throw new  RuntimeException("Erro ao gerar token JWT", exception);
         }
     }
 
