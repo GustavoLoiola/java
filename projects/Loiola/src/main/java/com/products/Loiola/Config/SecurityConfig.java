34ae925feaae08b0d40e407790206cbf63bd4fd1
GustavoLoiola <gusantanaloiola@gmail.com>
2026-09-12 17:07:30 -0300

    alteração no config
    removendo linha duplicada e adicionando anyRequest

--- projects/Loiola/src/main/java/com/products/Loiola/Config/SecurityConfig.java
+++ projects/Loiola/src/main/java/com/products/Loiola/Config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.products.Loiola.Config;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -38,10 +37,10 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/error").permitAll()
                         .requestMatchers(HttpMethod.POST, "/products/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
                 )
                 .addFilterBefore(
                         securityFilter,
